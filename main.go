622c998472bc5dd6d79552f0a5847083bed80cd1
alvin.yeoh <contact@alvinyeoh.com>
2021-06-18 16:46:12 +0800

    Create utils

--- main.go
+++ main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"log"
-	"os"
-
-	"github.com/joho/godotenv"
 	"github.com/mofodox/project-live-app/api/routes"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
 
 	"project-live-app/api/models"
+	"project-live-app/api/utils"
 )
 
 // db settings
@@ -23,22 +18,7 @@ var (
 )
 
 func init() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	} else {
-		dbHostname = os.Getenv("MYSQL_HOSTNAME")
-		dbPort = os.Getenv("MYSQL_PORT")
-		dbUsername = os.Getenv("MYSQL_USERNAME")
-		dbPassword = os.Getenv("MYSQL_PASSWORD")
-		dbDatabase = os.Getenv("MYSQL_DATABASE")
-
-		dbConnection = dbUsername + ":" + dbPassword + "@tcp(" + dbHostname + ":" + dbPort + ")/" + dbDatabase
-	}
-
-	db, _ := gorm.Open(mysql.Open(dbConnection), &gorm.Config{})
-
-	db.AutoMigrate(&models.Business{})
+	utils.Db.AutoMigrate(&models.Business{})
 }
 
 func main() {
