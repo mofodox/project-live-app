e522e80ad58f54da7b5942aa8198f71f427616a5
Khairul Akmal <khairulakmal.dev@gmail.com>
2021-06-22 23:09:28 +0800

    Update endpoint for login and logout

--- api/controllers/routes.go
+++ api/controllers/routes.go
@@ -8,8 +8,9 @@ func (server *Server) initializeRoutes() {
 	* TODO: Change the REST API Verbs
 	*/
 	server.Router.HandleFunc(defaultURI+"/users", server.Register).Methods("POST")
-	server.Router.HandleFunc(defaultURI+"/users", server.Login).Methods("POST")
+	server.Router.HandleFunc(defaultURI+"/users/login", server.Login).Methods("POST")
 	server.Router.HandleFunc(defaultURI+"/users", server.GetUsers).Methods("GET")
+	server.Router.HandleFunc(defaultURI+"/users/logout", server.Logout).Methods("POST")
 
 	/**
 	* API Health check route
