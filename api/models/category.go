f50079aea058696a069180141acb3a7639af4466
Smbsg <bisonslzx@gmail.com>
2021-06-22 22:07:12 +0800

    Updated category.go

--- api/models/category.go
+++ api/models/category.go
@@ -1,17 +1,15 @@
 package models
 
 import (
-	//"time"
-
-	"github.com/jinzhu/gorm"
+	"time"
 )
 
 type Category struct {
-	gorm.Model
-	ID          int    `json:"id"`
+	//gorm.Model
+	ID uint `gorm:"primary_key;not null;"`
+	//ParentID    uint   `gorm:"primary_key;not null;auto_increment:false"`
 	Name        string `json:"name"`
 	Description string `json:"description"`
-	// createdAt time.Time `json:"createdat"`
-	// updatedAt time.Time `json:"updatedat"`
-	// parentID int `json:"parentid"`
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
 }
