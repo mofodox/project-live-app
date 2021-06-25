05ed3255402a25e2495caad22297731cdce7cb2d
Smbsg <bisonslzx@gmail.com>
2021-06-25 18:06:19 +0800

    Added update

--- client/controllers/category_controller.go
+++ client/controllers/category_controller.go
@@ -7,7 +7,9 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/mofodox/project-live-app/api/models"
 )
 
@@ -29,7 +31,121 @@ func CreateCategoryPage(res http.ResponseWriter, req *http.Request) {
 }
 
 func ProcessCategoryForm(res http.ResponseWriter, req *http.Request) {
-	fmt.Println("Post Category Page")
+	categoryName := req.FormValue("name")
+	description := req.FormValue("description")
+
+	data, err := json.Marshal(map[string]string{
+		"name":        categoryName,
+		"description": description,
+	})
+	if err != nil {
+		log.Fatalf("login error %v\n", err)
+	}
+
+	respBody := bytes.NewBuffer(data)
+	fmt.Println(data)
+
+	// Todo: add cookie check and send JWT with request
+	client := &http.Client{}
+	request, _ := http.NewRequest(http.MethodPost, "http://localhost:8080/api/v1/categories", respBody)
+	fmt.Println(request)
+	request.Header.Set("Content-Type", "application/json")
+
+	response, err := client.Do(request)
+	fmt.Println(response)
+
+	if err != nil {
+		// handle error
+		fmt.Println("Category creation failed")
+		http.Redirect(res, req, "/", response.StatusCode)
+		fmt.Printf("The HTTP request failed with error %s\n", err)
+	} else {
+		data, _ := ioutil.ReadAll(response.Body)
+		fmt.Println(response.StatusCode)
+
+		// Success
+		if response.StatusCode == 201 {
+			fmt.Println("Category created successfully")
+			fmt.Println(string(data))
+			http.Redirect(res, req, "/", http.StatusOK)
+		} else {
+			// handle error
+			fmt.Println("Category creation failed")
+			http.Redirect(res, req, "/", response.StatusCode)
+		}
+	}
+}
+
+func UpdateCategory(res http.ResponseWriter, req *http.Request) {
+
+	fmt.Println("UPDATE CATEGORY FORM")
+
+	vars := mux.Vars(req)
+	_, err := strconv.Atoi(vars["id"])
+
+	if err != nil {
+		// Redirect to Index Page
+		http.Redirect(res, req, "/", http.StatusNotFound)
+		return
+	}
+
+	var category *models.Category
+
+	// Anonymous payload
+	payload := struct {
+		PageTitle  string
+		Category   *models.Category
+		ErrorMsg   string
+		SuccessMsg string
+	}{
+		"Update Category", nil, "", "",
+	}
+
+	// Todo: add cookie check and send JWT with request
+	client := &http.Client{}
+	fmt.Println(client)
+	request, _ := http.NewRequest(http.MethodGet, "http://localhost:8080/api/v1/categories/"+vars["id"], nil)
+	fmt.Println(request)
+	request.Header.Set("Content-Type", "application/json")
+
+	response, err := client.Do(request)
+	fmt.Println(response)
+
+	if err != nil {
+		// handle error
+		fmt.Println("Category update failed")
+		http.Redirect(res, req, "/", response.StatusCode)
+		fmt.Printf("The HTTP request failed with error %s\n", err)
+	} else {
+		fmt.Println(response.StatusCode)
+
+		data, _ := ioutil.ReadAll(response.Body)
+		marshalErr := json.Unmarshal(data, &category)
+
+		if marshalErr != nil {
+			fmt.Println("Error decoding json")
+		}
+
+		payload.Category = category
+
+		tpl.ExecuteTemplate(res, "updateCategory.gohtml", payload)
+		return
+	}
+}
+
+func ProcessUpdateCategory(res http.ResponseWriter, req *http.Request) {
+
+	fmt.Println("PROCESS UPDATE CATEGORY")
+
+	vars := mux.Vars(req)
+	_, err := strconv.Atoi(vars["id"])
+
+	if err != nil {
+		// Redirect to Index Page
+		http.Redirect(res, req, "/", http.StatusNotFound)
+		return
+	}
+
 	categoryName := req.FormValue("name")
 	description := req.FormValue("description")
 
@@ -45,62 +161,29 @@ func ProcessCategoryForm(res http.ResponseWriter, req *http.Request) {
 
 	// Todo: add cookie check and send JWT with request
 	client := &http.Client{}
-	request, _ := http.NewRequest(http.MethodPost, "http://localhost:8080/api/v1/categories", respBody)
+	request, _ := http.NewRequest(http.MethodPut, "http://localhost:8080/api/v1/categories/"+vars["id"], respBody)
 	request.Header.Set("Content-Type", "application/json")
 
 	response, err := client.Do(request)
 
 	if err != nil {
+		// handle error
+		fmt.Println("Category creation failed")
+		http.Redirect(res, req, "/", response.StatusCode)
 		fmt.Printf("The HTTP request failed with error %s\n", err)
 	} else {
 		data, _ := ioutil.ReadAll(response.Body)
 		fmt.Println(response.StatusCode)
 
 		// Success
-		if response.StatusCode == 201 {
-			fmt.Println("Category created successfully")
+		if response.StatusCode == 200 {
+			fmt.Println("Category updated successfully")
 			fmt.Println(string(data))
 			http.Redirect(res, req, "/", http.StatusOK)
 		} else {
 			// handle error
-			fmt.Println("Category creation failed")
+			fmt.Println("Category update failed")
+			http.Redirect(res, req, "/", response.StatusCode)
 		}
 	}
 }
-
-// func UpdateCategoryPage(res http.ResponseWriter, req *http.Request) {
-
-// 	fmt.Println("UPDATE BUSINESS FORM")
-
-// 	vars := mux.Vars(req)
-// 	category_id, err := strconv.Atoi(vars["id"])
-
-// 	if err != nil {
-// 		// Redirect to Index Page
-// 		http.Redirect(res, req, "/", http.StatusNotFound)
-// 		return
-// 	}
-
-// 	var category *models.Category
-
-// 	if err := server.DB.First(&category, category_id).Error; err != nil {
-// 		// Redirect to Index Page
-// 		http.Redirect(res, req, "/", http.StatusNotFound)
-// 		return
-// 	}
-
-// 	fmt.Println(category)
-
-// 	// Anonymous payload
-// 	payload := struct {
-// 		PageTitle  string
-// 		User       *models.User
-// 		Business   *models.Category
-// 		ErrorMsg   string
-// 		SuccessMsg string
-// 	}{
-// 		"Update Category", nil, category, "", "",
-// 	}
-
-// 	tpl.ExecuteTemplate(res, "updateCategory.gohtml", payload)
-// }
