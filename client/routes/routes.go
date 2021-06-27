23036eba7656a8a4b7ccc5554ae374e9b5aa461e
alvin.yeoh <contact@alvinyeoh.com>
2021-06-27 15:24:05 +0800

    Updated homepage to list businesses

--- client/routes/routes.go
+++ client/routes/routes.go
@@ -12,7 +12,7 @@ func HandleRoutes(addr string) {
 
 	router := mux.NewRouter().StrictSlash(true)
 
-	router.HandleFunc("/", controllers.Home)
+	router.HandleFunc("/", controllers.ListBusiness)
 	router.HandleFunc("/register", controllers.Register).Methods("POST", "GET")
 	router.HandleFunc("/login", controllers.Login).Methods("POST", "GET")
 
